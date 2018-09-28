687aacddb1e07820879636681497097caec36a96
Anton <huliovalente@gmail.com>
2018-09-28 21:58:50 +0300

    ver.3

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ module.exports = function solveEquation(equation) {
     var q = numbers[2]; // вопрос по впереди стоящему знаку
     var z = numbers[3];
     var c = +z;
-    var b = -q;
+    var b = +q;
     //var koren = Math.sqrt((b * b) - (4 * a * c));
     //var xx = (-b + koren) / 2 * a;
     //var yy = (-b - koren) / 2 * a;
-    var aa = b * (-1); //первая операция
+    var aa = b; //первая операция
     var bb = b * b; //вторая операция
     var cc = 4 * a * c; //третья операция
     var dd = Math.sqrt(bb - cc); // четвертая операция
@@ -22,6 +22,11 @@ module.exports = function solveEquation(equation) {
 
         //var x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
         //var x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
-    return [x1, x2];
+    if (x1 > x2) {
+        return [x2, x1];
+    }
+        else {
+            return [x1, x2];
+        }
     //return (aa);
 }
