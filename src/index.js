34b8fd1982826dcf3b12a04d6aae29bc60b03e12
Anton <huliovalente@gmail.com>
2018-09-28 23:43:17 +0300

    ver hren znaet kakaya

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 module.exports = function solveEquation(equation) {
     // your implementation
+    function isInteger(num) {
+        return (num ^ 0) === num;
+    }
     var string = equation;
-    var numbers = string.match(/[+-]?\d+(?:\.\d+)?/g).map(Number);
+    var result = string.replace(/\s/g, "");
+    var numbers = result.match(/[+-]?\d+(?:\.\d+)?/g).map(Number);
     var a = numbers[0];
-    var q = numbers[2]; // вопрос по впереди стоящему знаку
-    var z = numbers[3];
-    var c = +z;
-    var b = +q;
-    //var koren = Math.sqrt((b * b) - (4 * a * c));
-    //var xx = (-b + koren) / 2 * a;
-    //var yy = (-b - koren) / 2 * a;
+    var b = numbers[2]; // вопрос по впереди стоящему знаку
+    var c = numbers[3];
     var aa = b; //первая операция
     var bb = b * b; //вторая операция
-    var cc = 4 * a * c; //третья операция
+    var ca = 4 * a; //третья операция
+    var cc = ca * c;
     var dd = Math.sqrt(bb - cc); // четвертая операция
     var e1 = aa + dd; //числитель х1
     var e2 = aa - dd;//числитель х2
     var x1 = e1 / (2 * a);
     var x2 = e2 / (2 * a);
-
-
-        //var x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
-        //var x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / 2 * a;
-    if (x1 > x2) {
-        return [x2, x1];
+    var x3 = x1 * (-1);
+    var x4 = x2 * (-1);
+    if (x3 > x4) {
+        return [x4, x3];
     }
-        else {
-            return [x1, x2];
-        }
-    //return (aa);
+    else {
+        return [x3, x4];
+    }
 }
